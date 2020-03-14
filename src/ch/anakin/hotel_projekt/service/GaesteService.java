4b608d3a6506ddb1aa30a5cd10162f4aef69ba49
Anakin Kirschler <kirschlera@bzz.ch>
2020-03-14 22:28:05 +0100

    Update Service fertig

--- src/ch/anakin/hotel_projekt/service/GaesteService.java
+++ src/ch/anakin/hotel_projekt/service/GaesteService.java
@@ -4,15 +4,10 @@ import ch.anakin.hotel_projekt.data.DataHandler;
 import ch.anakin.hotel_projekt.model.Gast;
 import ch.anakin.hotel_projekt.model.Hotel;
 
-import javax.annotation.PostConstruct;
 import javax.validation.Valid;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.crypto.Data;
-import java.util.UUID;
 import java.util.Vector;
 
 /**
@@ -108,6 +103,37 @@ public class GaesteService {
             if (gast != null) {
                 httpStatus = 200;
                 hotel.getGaesteListe().remove(gast);
+                DataHandler.writeBooks(hotel.getGaesteListe());
+            } else {
+                httpStatus = 404;
+            }
+        } catch (IllegalArgumentException argEx) {
+            httpStatus = 400;
+        }
+
+        Response response = Response
+                .status(httpStatus)
+                .entity("")
+                .build();
+        return response;
+    }
+
+
+    @PUT
+    @Path("update")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateBook(
+            @Valid @BeanParam Gast gast,
+            @QueryParam("schlüsselwort") String schlüsselwort
+
+    ) {
+        int httpStatus;
+        try {
+            Hotel hotel = new Hotel();
+            gast = hotel.getGast(schlüsselwort);
+            if (gast != null) {
+                httpStatus = 200;
+
                 DataHandler.writeBooks(hotel.getGaesteListe());
             } else {
                 httpStatus = 404;
